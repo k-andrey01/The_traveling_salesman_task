da72bf31c16e7e2f1264a2e99e88fbdbec515645
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2022-11-22 19:17:56 +0300

    Удалены ненужные классы

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -8,7 +8,5 @@ public class MyGraph {
                           {79, 46, 21, 37, -1, 38},
                           {39, 14, 40, 6, 38, -1}};
         myFinder.find(matrix);
-        Finder2 myFinder2 = new Finder2();
-        //myFinder2.find(matrix);
     }
 }
